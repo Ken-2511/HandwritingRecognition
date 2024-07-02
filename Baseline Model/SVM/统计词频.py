e08feb63fcda65a0c25da84209209dcfb00159c2
程永康 <IWMAIN@outlook.com>
2024-07-02 20:27:15 +0800

    write into csv file

--- Baseline Model/SVM/统计词频.py
+++ Baseline Model/SVM/统计词频.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import numpy as np
+import csv
 
 
 def extract_tensor(pt_directory):
@@ -43,17 +44,29 @@ pt_directory_IAM = '/root/autodl-tmp/APS360_Project/Datasets/IAM_Processed'
 # print(count_word_num(create_word_list(extract_tensor(pt_directory_CVL))))
 # print(count_word_num(create_word_list(extract_tensor(pt_directory_IAM))))
 
-result = count_word_num(create_word_list(extract_tensor(pt_directory_CVL)))
-sorted_result = sorted(result.items(), key=lambda x: x[1], reverse=True)
+result_CVL = count_word_num(create_word_list(extract_tensor(pt_directory_CVL)))
+sorted_result_CVL = sorted(result_CVL.items(), key=lambda x: x[1], reverse=True)
 
 classified = 0
 threshold = 300
-for i in sorted_result[:threshold]:
+for i in sorted_result_CVL[:threshold]:
     classified += i[1]
     print(i)
 others = 0
-for i in sorted_result[threshold:]:
+for i in sorted_result_CVL[threshold:]:
     others += i[1]
 print(('classified', classified))
 print(('others', others))
-print(f"total classes: {len(sorted_result)}")
+print(f"total classes: {len(sorted_result_CVL)}")
+
+# # 创建并写入CSV文件
+# with open('one_hot_map.csv', 'w', newline='', encoding='utf-8') as csvfile:
+#     writer = csv.writer(csvfile)
+#     for i in sorted_result_CVL:
+#         writer.writerow(i)
+#     writer.writerow(('others',others))
+
+indexed_list = [(i+1, key, value) for i, (key, value) in enumerate(result_CVL.items())]
+total_word =  create_word_list(extract_tensor(pt_directory_CVL))
+for i in total_word:
+    i = 

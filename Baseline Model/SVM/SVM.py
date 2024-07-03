9c7efae8b417951717a59db93ab409dbd1d5540d
程永康 <IWMAIN@outlook.com>
2024-07-03 15:24:43 +0800

    change dataset from IAM to CVL

--- Baseline Model/SVM/SVM.py
+++ Baseline Model/SVM/SVM.py
@@ -11,12 +11,13 @@ from skimage.feature import hog
 import os
 import torch
 import csv
+import numpy as np
 
 # CSV文件路径
 csv_file_path = '/root/autodl-tmp/CVL_indices.csv'
 
 # 初始化一个空列表，用于存储CSV文件中的每一行
-y_CVL = []
+label_list_CVL = []
 
 # 打开CSV文件
 with open(csv_file_path, newline='') as csvfile:
@@ -26,7 +27,8 @@ with open(csv_file_path, newline='') as csvfile:
     # 遍历csv阅读器中的每行
     for row in reader:
         # 将每行的数据添加到列表中
-        y_CVL.append(row)
+        label_list_CVL.append(row)
+y_CVL = np.array(label_list_CVL).reshape(-1,1)
 
 def convert_png_to_hog_tensor(png_directory):
     hog_features = []
@@ -51,36 +53,36 @@ def convert_png_to_hog_tensor(png_directory):
     return hog_tensor
 
 # 加载数据集
-pt_directory_CVL = '/root/autodl-tmp/APS360_Project/Baseline Model/SVM/CVL_HOG_pt'
-pt_directory_IAM = '/root/autodl-tmp/APS360_Project/Baseline Model/SVM/IAM_HOG_pt'
+pt_directory_CVL = '/root/autodl-tmp/APS360_Project/Baseline Model/SVM/CVL_dataset_png'
+pt_directory_IAM = '/root/autodl-tmp/APS360_Project/Baseline Model/SVM/IAM_dataset_png'
 
 
 X_CVL = convert_png_to_hog_tensor(pt_directory_CVL)
-X_IAM = convert_png_to_hog_tensor(pt_directory_IAM)
+#X_IAM = convert_png_to_hog_tensor(pt_directory_IAM)
 
-y_IAM = []
+# y_IAM = []
 
 
 # 划分训练集和测试集
 X_CVL_train, X_CVL_test, y_CVL_train, y_CVL_test = train_test_split(X_CVL, y_CVL, test_size=0.3, random_state=42)
-X_IAM_train, X_IAM_test, y_IAM_train, y_IAM_test = train_test_split(X_IAM, y_IAM, test_size=0.3, random_state=42)
+#X_IAM_train, X_IAM_test, y_IAM_train, y_IAM_test = train_test_split(X_IAM, y_IAM, test_size=0.3, random_state=42)
 
 # 数据标准化
 scaler = StandardScaler()
 X_CVL_train = scaler.fit_transform(X_CVL_train)
 X_CVL_test = scaler.transform(X_CVL_test)
-X_IAM_train = scaler.fit_transform(X_IAM_train)
-X_IAM_test = scaler.transform(X_IAM_test)
+# X_IAM_train = scaler.fit_transform(X_IAM_train)
+# X_IAM_test = scaler.transform(X_IAM_test)
 # 建立 SVM 模型
 svm_CVL = SVC(kernel='linear', decision_function_shape='ovo')  # 使用一对一方法
-svm_IAM = SVC(kernel='linear', decision_function_shape='ovo')
+# svm_IAM = SVC(kernel='linear', decision_function_shape='ovo')
 # 训练模型
 svm_CVL.fit(X_CVL_train, y_CVL_train)
-svm_IAM.fit(X_IAM_train, y_IAM_train)
+# svm_IAM.fit(X_IAM_train, y_IAM_train)
 
 # 预测
 y_pred_CVL = svm_CVL.predict(X_CVL_test)
-y_pred_IAM = svm_IAM.predict(X_IAM_test)
+# y_pred_IAM = svm_IAM.predict(X_IAM_test)
 
 # 评估模型
 def evaluate(y_test,y_pred):
